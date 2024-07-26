47a8dad7ca85ef732e01fe754bcc0144d5e42735
rhasan <rhasan@BM1301>
2024-07-26 19:40:09 +0530

    investment

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import { useState } from 'react';
+import Results from './components/Results';
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+const inputIsValid = userInput.duration >= 1; 
+
+function handleChange(inputIdentifier, newValue){
+    setUserInput((preUserInput)=>{
+        return {
+            ...preUserInput,
+            [inputIdentifier]: +newValue
+        }
+    })
+}
+  return (
+    <>
+      <Header/>
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputIsValid && <p className='center'>Please enter a valid input data.</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  )
+}
+
+export default App
